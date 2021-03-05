740af4e9edc8234784b85f281531182dfbf65e37
Daniel Moradiellos <daniel.moradiellos@b34tz.net>
2021-03-05 18:57:27 +0100

    date ven 05 mar 2021 18:51:17 CET : debug long unsigned int comparison : ./PHARMACIE_VANDERKINDER_ROLLING.cpp: In function ‘int main()’: ./PHARMACIE_VANDERKINDER_ROLLING.cpp:9:19: warning: comparison of integer expressions of different signedness: ‘int’ and ‘std::__cxx11::basic_string<char>::size_type’ {aka ‘long unsigned int’} [-Wsign-compare]     9 |  for( int i=0 ; i < s.length();i++)       |                 ~~^~~~~~~~~~~~

--- PHARMACIE_VANDERKINDER_ROLLING.cpp
+++ PHARMACIE_VANDERKINDER_ROLLING.cpp
@@ -6,7 +6,7 @@ int main(void)
 	std::string s = "PHARMACIE VANDERKINDERE";
 	std::string sub_str = s;
 	int Windowsize = 5;
-	for(int i=0 ; i < s.length();i++)
+	for(long unsigned int i=0 ; i < s.length();i++)
 	{
 		sub_str = s.substr(i,Windowsize);
 //		std::cout << "i = " << i << " and i + Windowsize = "  <<  i + Windowsize << std::endl; 
